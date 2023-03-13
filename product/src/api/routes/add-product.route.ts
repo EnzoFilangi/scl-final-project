3632f42505334d72babbe8e941677d29c88238f1
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-13 17:08:22 +0100

    POST product route

--- product/src/api/routes/add-product.route.ts
+++ product/src/api/routes/add-product.route.ts
@@ -4,6 +4,21 @@ import {ProductEntity} from "../../persistence/entities/product.entity";
 
 export function addProductRouteFactory(productRepository : Repository<ProductEntity>){
     return async (req: Request, res: Response) => {
+        const product: { name ?: any, price ?: any } = req.body;
 
+        if (!product.name || !product.name){
+            res.sendStatus(400);
+            return;
+        }
+
+        if (!(typeof product.name === 'string') || !(typeof product.price === 'number')){
+            res.sendStatus(400);
+            return;
+        }
+
+        const newProduct = new ProductEntity(product.name, product.price);
+        await productRepository.save(newProduct);
+
+        res.sendStatus(201);
     }
 }
