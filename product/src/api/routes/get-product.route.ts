49a62ed3b3f9aa9b55c1401dbf067fbfd1bef9d9
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-13 17:04:42 +0100

    GET product route

--- product/src/api/routes/get-product.route.ts
+++ product/src/api/routes/get-product.route.ts
@@ -3,7 +3,18 @@ import {Repository} from "typeorm";
 import {ProductEntity} from "../../persistence/entities/product.entity";
 
 export function getProductRouteFactory(productRepository : Repository<ProductEntity>){
-    return (req: Request, res: Response) => {
+    return async (req: Request, res: Response) => {
+        const productId: string = req.query['productId'] as string;
 
+        if (!productId) {
+            res.sendStatus(400);
+            return;
+        }
+
+        const product = await productRepository.findOneBy({
+            id: productId
+        })
+
+        res.json(product)
     }
 }
