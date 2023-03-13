20cf5230e0aa0f1041f86836447e37b5bfe79b0a
EnzoFilangi <enzo.filangi@efrei.net>
2023-03-13 17:11:05 +0100

    DELETE product route

--- product/src/api/routes/delete-product.route.ts
+++ product/src/api/routes/delete-product.route.ts
@@ -4,6 +4,17 @@ import {ProductEntity} from "../../persistence/entities/product.entity";
 
 export function deleteProductRouteFactory(productRepository : Repository<ProductEntity>){
     return async (req: Request, res: Response) => {
+        const productId: string = req.query['productId'] as string;
 
+        if (!productId) {
+            res.sendStatus(400);
+            return;
+        }
+
+        await productRepository.delete({
+            id: productId
+        })
+
+        res.sendStatus(200);
     }
 }
